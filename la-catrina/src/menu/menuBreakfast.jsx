102dacf3b6293d001f5fb12ab3c5a87d8e37458b
Xiomara <xiomarach2000@gmail.com>
2021-10-28 18:12:09 -0500

    Se comienza con el llamado de data y el uso de componentes.

--- la-catrina/src/menu/menuBreakfast.jsx
+++ la-catrina/src/menu/menuBreakfast.jsx
@@ -1,17 +1,18 @@
 import './menu.scss';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import logoWhite from '../img/burrito-blanco.png';
 import add from '../img/mas.png';
+//import products from'../data.json'
 
 const MenuBreakfast = () => (
     <Fragment>
-        <main className='sect-menu'>
+        <section className='sect-menu'>
             <header >
                 <a href='/'><img className='logo-white' src={logoWhite} alt='logo' /></a>
                 <a href='/menu'><img className='add-order' src={add} alt='logo' /></a>
             </header>
-            <div className='container-menu'>
-                <section className='sect-input'>
+            <main className='container-menu'>
+                <div className='sect-input'>
                     <input type='text' placeholder='Nombre' className='input-name'></input>
                     <select name='' className='num-table'>
                         <option disabled selected>Mesa</option>
@@ -21,25 +22,52 @@ const MenuBreakfast = () => (
                         <option value=''>Mesa 4</option>
                         <option value=''>Mesa 5</option>
                     </select>
-                </section>
-                <section className='sect-buttons'>
+                </div>
+                <div className='sect-buttons'>
                     <a href='/menu' className='btn-principal'>Principal</a>
                     <a href='/break-menu' className='btn-breakfast'>Desayunos</a>
-                </section>
-                <div className='products'>
-                    <section className='food-break'>
-                        <h3>Comidas</h3>
-                    </section>
-                    <section className='drinks-break'>
-                        <h3>Bebidas</h3>
-                    </section>
                 </div>
+                {/* <div className='products'>
+                    <div className='food-break'>
+                        <h3>Comidas</h3>
+                    </div>
+                    <div className='drinks-break'>
+                        <h3>Bebidas</h3>
+                    </div>
+                </div> */}
                 <nav className = 'go-order'>
                     <a href='/order' className='btn-order'>Ir a Pedidos</a>
                 </nav>
-            </div>
-        </main>
-    </Fragment>
+            </main>
+        </section>
+    </Fragment> 
 );
+
+const ProductCategory = ({category}) => {
+    fetch
+    return (<div className='products'>
+    <div className='food-break'>
+        <h3>{category}</h3>
+    </div>
+    <div className='drinks-break'>
+        <h3>{category}</h3>
+    </div>
+</div>)
+}
+
+/*const ProductsMenu = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetch("../data.json")
+        .then(res => res.json())
+        .then(datos => {
+            setProducts(datos)
+        }) 
+    }, [])
+    return products;
+}
+export default Products = () => {
+    const products = ProductsMenu
+};*/
 
 export default MenuBreakfast;
