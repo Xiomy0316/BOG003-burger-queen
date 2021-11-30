1e4f1998dca45198c75865621a2dc53a29f28f37
tolozayurany <yalbarracint@gmail.com>
2021-11-30 18:19:15 -0500

    Se crea resumen de precios con adicionales en Order Summary y creación de estilos para resumen de pedido

--- la-catrina/src/menu/modalMenu.jsx
+++ la-catrina/src/menu/modalMenu.jsx
@@ -1,29 +1,29 @@
 import { Fragment, useState } from "react";
 
 const ModalMenu = ({ setOpenModal, burritoProduct, addOrder, setAddOrder }) => {
-    const [typeOfProtein, setTypeOfProtein] = useState('res');
+    const [typeOfProtein, setTypeOfProtein] = useState('Res');
     const [productAditionals, setProductAditionals] = useState([]);
-    console.log(addOrder, 'afuera');
+    // console.log(productAditionals, 'afuera');
 
     const handleValueProtein = e => {
         setTypeOfProtein(e.target.value)
     }
 
     const handleValueAditionals = e => {
-        const searchPosition = productAditionals.findIndex(item => item === e.target.value)
-        console.log('Linea19', productAditionals);
+        const searchPosition = productAditionals.findIndex(item => item.name === e.target.value)
         let newProduct = productAditionals;
-        if (productAditionals.includes(e.target.value)) {
+
+        if (searchPosition !== -1) {
             newProduct.splice(searchPosition, 1)
             setProductAditionals(newProduct)
-            console.log('if', e.target.value, productAditionals);
+            //console.log('if', e.target.value, productAditionals);
         } else {
             setProductAditionals(productAditionals => [...productAditionals, { name: e.target.value, price: 1 }])
-            console.log('else', e.target.value, productAditionals);
+            //console.log('else', e.target.value, productAditionals);
         }
     }
 
-    const sendBurritoToObject = () => setAddOrder([...addOrder, { ...burritoProduct, protein: typeOfProtein, additional: productAditionals, amount: 1}])
+    const sendBurritoToObject = () => setAddOrder([...addOrder, { ...burritoProduct, protein: typeOfProtein, additional: productAditionals, amount: 1 }])
 
     return (
         <Fragment>
@@ -37,27 +37,27 @@ const ModalMenu = ({ setOpenModal, burritoProduct, addOrder, setAddOrder }) => {
                         <ul>Proteína:
                             <li><input
                                 id="protein1"
-                                value="res"
+                                value="Res"
                                 type="radio"
-                                checked={typeOfProtein === 'res' ? true : false}
+                                checked={typeOfProtein === 'Res' ? true : false}
                                 onChange={handleValueProtein} />
                                 <label form="protein1">
                                     {burritoProduct.protein[0]}
                                 </label></li>
                             <li><input
                                 id="protein2"
-                                value="pollo"
+                                value="Pollo"
                                 type="radio"
-                                checked={typeOfProtein === 'pollo' ? true : false}
+                                checked={typeOfProtein === 'Pollo' ? true : false}
                                 onChange={handleValueProtein} />
                                 <label form="protein2">
                                     {burritoProduct.protein[1]}
                                 </label></li>
                             <li><input
                                 id="protein3"
-                                value="vegetariano"
+                                value="Vegano"
                                 type="radio"
-                                checked={typeOfProtein === 'vegetariano' ? true : false}
+                                checked={typeOfProtein === 'Vegano' ? true : false}
                                 onChange={handleValueProtein} />
                                 <label form="protein3">
                                     {burritoProduct.protein[2]}
@@ -67,14 +67,14 @@ const ModalMenu = ({ setOpenModal, burritoProduct, addOrder, setAddOrder }) => {
                             <li><input
                                 type="checkbox"
                                 id="aditional1"
-                                value="queso"
+                                value="Queso"
                                 onChange={handleValueAditionals} />
                                 {burritoProduct.additional[0].name}
                                 $ {burritoProduct.additional[0].price}</li>
                             <li><input
                                 type="checkbox"
                                 id="aditional2"
-                                value="jalapeños"
+                                value="Jalapeños"
                                 onChange={handleValueAditionals} />
                                 {burritoProduct.additional[1].name}
                                 $ {burritoProduct.additional[1].price}</li>
--- la-catrina/src/menu/orderSummary.jsx
+++ la-catrina/src/menu/orderSummary.jsx
@@ -2,17 +2,21 @@ import { Fragment } from "react";
 
 const OrderSummary = ({ addOrder }) => {
 
-    //console.log(addOrder, 'hola');
 
-    /* Si el precio está definido retorna el precio multiplicado por cantidad en cada producto, si no retorna 0 */
     const priceProducts = addOrder.map(product => {
-        if(product.category === 'Comidas' && product.type === 'lunch') {
-           let sumPrice =  product.additional.reduce((additional, sumPriceAdditional) => additional.price + sumPriceAdditional, 0)
-
-            console.log(sumPrice)
-            }
-    }) /* product.price !== undefined ? product.price * product.amount : 0) */
-
+        let totalPriceAdditional;
+        /* Valida si el producto es burrito y realiza la suma de sus adicionales + el precio del producto */
+        if (product.category === 'Comidas' && product.type === 'lunch') {
+            totalPriceAdditional = product.additional.map(additional => additional.price)
+                .reduce((sum, value) => (sum + value), 0);
+            let sumProductAdditional = (product.price * product.amount) + totalPriceAdditional;
+            return sumProductAdditional;
+            /* Si el precio está definido retorna el precio multiplicado por cantidad en cada producto, si no retorna 0 */
+        } if (product.price !== undefined) {
+            return product.price * product.amount;
+        }
+        return 0;
+    })
     /* Retorna la suma acumulada de prirceProducts para hallar el precio total del pedido */
     const totalOrderPrice = priceProducts.reduce((price, sumPrice) => price + sumPrice, 0);
 
@@ -21,19 +25,20 @@ const OrderSummary = ({ addOrder }) => {
             <section>
                 <h3>Resumen de pedido</h3>
                 {addOrder.map(product =>
-                    <div key={product.id} className='product-summary'>
-                        <section>
+                    <div key={product.id} >
+                        <section className='product-summary'>
                             <p>{product.name}</p>
+                            <p>{product.protein}</p>
                             <p>{product.amount}</p>
                             <p>$ {product.price !== undefined ? product.price * product.amount : ''}</p>
                         </section>
-                        <section>
-                            {product.category === 'Comidas' && product.type === 'lunch' ? product.additional.map(additional =>
-                                <div>
-                                    <p>{additional.name}</p>
-                                    <p>$ {additional.price}</p> </div>)
-                                : ''}
-                        </section>
+                        
+                        {product.category === 'Comidas' && product.type === 'lunch' ? product.additional.map(additional =>
+                            <div className='additional-burritos'>
+                                <p>•{additional.name}</p>
+                                <p>$ {additional.price}</p>
+                            </div>)
+                            : ''}
                     </div>
                 )}
                 <div className='total-order-price'>
