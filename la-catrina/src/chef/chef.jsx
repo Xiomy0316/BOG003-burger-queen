bb20d980e084b61f47614143fb2a6f5be16b81f3
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-12-07 18:59:57 -0500

    Se realizan estilos del componente chef

--- la-catrina/src/chef/chef.jsx
+++ la-catrina/src/chef/chef.jsx
@@ -1,7 +1,8 @@
 import { Fragment, useState, useEffect } from 'react';
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import db from '../firebase/firebaseConfig';
-import Order from './order'
+import OrderChef from './orderChef';
+import logoWhite from '../img/burrito-blanco.png';
 import './chef.scss';
 //import Timer from './timer';
 
@@ -30,10 +31,14 @@ const Chef = () => {
   return (
     <Fragment>
       <section className='orders-chef'>
-        <a href='/'>Ir a home</a>
-        {ordersData.filter(order => (order.state !== 'Entregado' && order.state !== 'Para entregar')).map((orderObject) => (
-        <Order orderObject = {orderObject}/>))
-        }
+        <header className='order-header'>
+          <a href='/'><img className='logo-white' src={logoWhite} alt='logo' /></a>
+        </header>
+        <section className='orders-chef-container'>
+          {ordersData.filter(order => (order.state !== 'Entregado' && order.state !== 'Para entregar')).map((orderObject) => (
+            <OrderChef orderObject={orderObject} />))
+          }
+        </section>
       </section>
     </Fragment>
   )
---
+++ la-catrina/src/chef/orderChef.jsx
@@ -0,0 +1,111 @@
+import db from '../firebase/firebaseConfig';
+import { updateDoc, doc } from "firebase/firestore";
+import { Fragment, useState, useEffect } from 'react';
+
+const OrderChef = (orderObject) => {
+    const updateStateOrder = (order) => {
+        const stateOrder = doc(db, "pedidos", order.id);
+        if (order.state === 'Enviar a cocina') {
+            return updateDoc(stateOrder, {
+                state: 'En preparación'
+            });
+        }
+        if (order.state === 'En preparación') {
+            return updateDoc(stateOrder, {
+                state: 'Para entregar'
+            });
+        }
+    }
+
+    const prueba = (product) => {
+        return Object.values(product).filter(item => typeof item === 'object')
+    }
+
+    const [diff, setDiff] = useState(null);
+    const [initial, setInitial] = useState(null);
+
+    const time = () => {
+        setDiff(new Date(+new Date() - initial))
+    };
+
+    const startTime = () => { setInitial(+new Date()) }
+
+    useEffect(() => {
+        if (initial) {
+            requestAnimationFrame(time);
+        }
+    }, [initial]);
+
+    useEffect(() => {
+        if (diff) {
+            requestAnimationFrame(time);
+        }
+    }, [diff]);
+
+    return (
+        <Fragment>
+            {Object.values(orderObject).map((product) =>
+                <div key={product.id} className='card-order'>
+                    {console.log(product.state)}
+                    <div className='timer' >
+                        <h3>{timeFormat(diff)} </h3>
+                    </div>
+                    {/* <div><Timer stateOrder={orderObject.state} /> </div> */}
+                    <section className='info-order'>
+                        <p>{product.personName}</p>
+                        <p>{product.tableSelect}</p>
+                    </section>
+                    <section className='line-orders-section'>
+                        <canvas className='line-orders'> </canvas>
+                    </section>
+                    <>{
+                        prueba(product).map((productInOrder) =>
+                            <div className='container-product-order'>
+                                <div key={productInOrder.id} className='product-order'>
+                                    <p> {productInOrder.name}</p>
+                                    <p className='num-order'> {productInOrder.protein}</p>
+                                    <p className='num-order'> {productInOrder.amount}</p>
+                                </div>
+                                {productInOrder.category === 'Comidas' && productInOrder.type === 'lunch' ? productInOrder.additional.map(additional =>
+                                    <div className='additionals-order'>
+                                        <p>•{additional.name}</p>
+                                    </div>)
+                                    : ''}
+
+                            </div>
+                        )}</>
+                    <section className='btn-deliver'>
+                        {product.state === 'Enviar a cocina'
+                            ? <button onClick={() => {
+                                updateStateOrder(product).then(() => {
+                                    startTime()
+                                })
+                            }}>
+                                Preparar
+                            </button> : ''}
+                        {product.state === 'En preparación'
+                            ? <button onClick={() => updateStateOrder(product)}>
+                                Para entregar
+                            </button> : ''}
+                    </section>
+                </div>
+            )
+            }
+        </Fragment>
+    )
+}
+
+const timeFormat = (date) => {
+    if (!date) return '00:00';
+
+    let mm = date.getUTCMinutes();
+    let ss = date.getSeconds();
+
+    mm = mm < 10 ? '0' + mm : mm;
+    ss = ss < 10 ? '0' + ss : ss;
+
+    return `${mm}:${ss}`;
+
+}
+
+export default OrderChef;
