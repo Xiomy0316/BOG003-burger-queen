505c4a8b4fa7f323476d5bf35d5d2a1da65b6a94
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-12-10 08:17:47 -0500

    Se realizan cambios en la funcion del cronometro en oerderChef

--- src/chef/orderChef.jsx
+++ src/chef/orderChef.jsx
@@ -21,34 +21,46 @@ const OrderChef = (orderObject) => {
         return Object.values(product).filter(item => typeof item === 'object')
     }
 
-    const [diff, setDiff] = useState(null);
-    const [initial, setInitial] = useState(null);
+    const [time, setTime] = useState(0);
+    const [timeOn, setTimeOn] = useState(false);
+    /*  const time = () => {
+         setDiff(new Date(+new Date() - initial))
+     }; */
 
-    const time = () => {
-        setDiff(new Date(+new Date() - initial))
-    };
-
-    const startTime = () => { setInitial(+new Date()) }
+    /*   const startTime = () => { setInitial(+new Date()) } */
 
     useEffect(() => {
-        if (initial) {
-            requestAnimationFrame(time);
+        let interval = null;
+        if (timeOn) {
+            interval = setInterval(() => {
+                setTime(prevTime => prevTime + 10)
+            }, 10)
+        } else {
+            clearInterval(interval)
         }
-    }, [initial]);
+        return () => clearInterval(interval)
+    }, [timeOn]);
 
-    useEffect(() => {
+    /* useEffect(() => {
         if (diff) {
             requestAnimationFrame(time);
         }
     }, [diff]);
-
+ */
     return (
         <Fragment>
             {Object.values(orderObject).map((product) =>
                 <div key={product.id} className='card-order'>
-                    {console.log(product.state)}
                     <div className='timer' >
-                        <h3>{timeFormat(diff)} </h3>
+                        <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}: </span>
+                        <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)} </span>
+                    </div>
+                    <div>
+                    <button onClick={() => 
+                                    setTimeOn(true)
+                            }>
+                                Cronometro
+                            </button>
                     </div>
                     {/* <div><Timer stateOrder={orderObject.state} /> </div> */}
                     <section className='info-order'>
@@ -78,7 +90,7 @@ const OrderChef = (orderObject) => {
                         {product.state === 'Enviar a cocina'
                             ? <button onClick={() => {
                                 updateStateOrder(product).then(() => {
-                                    startTime()
+                                    setTimeOn(true)
                                 })
                             }}>
                                 Preparar
@@ -95,7 +107,7 @@ const OrderChef = (orderObject) => {
     )
 }
 
-const timeFormat = (date) => {
+/* const timeFormat = (date) => {
     if (!date) return '00:00';
 
     let mm = date.getUTCMinutes();
@@ -106,6 +118,6 @@ const timeFormat = (date) => {
 
     return `${mm}:${ss}`;
 
-}
+} */
 
 export default OrderChef;
