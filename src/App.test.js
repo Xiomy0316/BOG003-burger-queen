2140ab7a302f313c3c325bc59e7c3725c3fb2794
Diana Buitrago <dianabuitrago54@hotmail.es>
2021-12-16 17:59:03 -0500

    Realizamos readme, se cargan fotos de prototipo y resultado final

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import Home from './home/home.js';
+import Home from './home/home.jsx';
 
 test('renders learn react link', () => {
   render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/La Catrina/i);
   expect(linkElement).toBeInTheDocument();
 });
